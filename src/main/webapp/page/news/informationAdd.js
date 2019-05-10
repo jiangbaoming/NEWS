d127db751f23fbe77cb822ca9aaa6a35718805dd
jiangming <837639305@qq.com>
2019-05-10 17:27:19 +0800

    step1

--- src/main/webapp/page/news/informationAdd.js
+++ src/main/webapp/page/news/informationAdd.js
@@ -60,31 +60,33 @@ layui.use(['form', 'layer', 'layedit', 'upload'], function () {
     form.on("submit(addNews)", function (data) {
         //弹出loading
         const index = top.layer.msg('数据提交中，请稍候', {icon: 16, time: false, shade: 0.8});
-        $.ajax({
-            url:"/news",
-            type: "POST",
-            dataType: "json",
-            data: {
-                title: $(".articleTitle").val(),
-                introduction: $(".introduction").val(),
-                category: $(".category").val(),
-                banner: coverUrl.src,
-                content: layedit.getContent(editIndex)
-            },
-            success: function (result) {
-                if (result.status ===200) {
-                    layer.msg("新增成功");
-                    setTimeout(function () {
-                        top.layer.close(index);
-                        layer.closeAll("iframe");
-                        //刷新父页面
-                        parent.location.reload();
-                    }, 500);
-                } else {
-                    layer.alert("失败！！！！", {icon: 7, anim: 6});
+        setTimeout(function () {
+            $.ajax({
+                url:"/news",
+                type: "POST",
+                dataType: "json",
+                data: {
+                    title: $(".articleTitle").val(),
+                    introduction: $(".introduction").val(),
+                    category: $(".category").val(),
+                    banner: coverUrl.src,
+                    content: layedit.getContent(editIndex)
+                },
+                success: function (result) {
+                    if (result.status ===200) {
+                        layer.msg("新增成功");
+                        setTimeout(function () {
+                            top.layer.close(index);
+                            layer.closeAll("iframe");
+                            //刷新父页面
+                            parent.location.reload();
+                        }, 1000);
+                    } else {
+                        layer.msg("新增失败!");
+                    }
                 }
-            }
-        });
+            });
+        },5000);
         return false;
     });
 });
