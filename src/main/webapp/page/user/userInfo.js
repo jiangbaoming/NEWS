2950e5a9f94712e68ce6e196e93f0ecae1d5ee4e
jiangbaoming <837639305@qq.com>
2019-05-13 20:07:20 +0800

    5

--- src/main/webapp/page/user/userInfo.js
+++ src/main/webapp/page/user/userInfo.js
@@ -1,11 +1,51 @@
-var form, $, areaData;
 layui.config({
     base: "/statics/js/"
+}).extend({
+    "treeSelect": "treeSelect"
 });
-layui.use(['form', 'layer'], function () {
-    var form = layui.form,
-        $ = layui.jquery,
-        layer = parent.layer === undefined ? layui.layer : top.layer;
+layui.use(['form', 'layer', 'treeSelect'], function () {
+    const form = layui.form,
+        layer = layui.layer,
+        treeSelect = layui.treeSelect;
+
+    var modifyOid = $("#tree").val();
+
+    treeSelect.render({
+        // 选择器
+        elem: '#tree',
+        // 数据
+        data: '/organization?method=getAll',
+        // 异步加载方式：get/post，默认get
+        type: 'get',
+        // 占位符
+        //placeholder: '请选择部门',
+        // 一些可定制的样式
+        style: {
+            folder: {
+                enable: true
+            },
+            line: {
+                enable: true
+            }
+        },
+        // 点击回调
+        click: function (d) {
+            //console.log(d);
+            modifyOid = d.current.id;
+            // layer.msg(updataDid);
+        },
+//      加载完成后的回调函数
+        success: function (d) {
+//               console.log(d);
+//                选中节点，根据id筛选
+            treeSelect.checkNode('tree', modifyOid);
+//                获取zTree对象，可以调用zTree方法
+//               var treeObj = treeSelect.zTree('tree');
+//               console.log(treeObj);
+//                刷新树结构
+            treeSelect.refresh('tree');
+        }
+    });
 
     $(function () {
         $.ajax({
@@ -26,23 +66,32 @@ layui.use(['form', 'layer'], function () {
 
     //提交个人资料
     form.on("submit(changeUser)", function (data) {
+        const field = data.field;
         var index = layer.msg('提交中，请稍候', {icon: 16, time: false, shade: 0.8});
         $.ajax({
-            url: $.cookie("tempUrl") + "admin/updateByPhone?token=" + $.cookie("token") + "&phone=" + $(".phone").val(),
-            type: "put",
-            dataType:"json",
+            url: "/user",
+            type: "post",
+            dataType: "json",
+            data: {
+                "method": "update",
+                "id": field.id,
+                "phone": field.phone,
+                "userName": field.userName,
+                "role": field.role,
+                "oid": modifyOid
+            },
             success: function (result) {
-                if (result.httpStatus === 200) {
+                if (result.code === 200) {
                     layer.msg("更新成功,请重新登陆...");
                     setTimeout(function () {
                         top.layer.close(index);
                         layer.closeAll("iframe");
                         //跳转至登陆界面
                         $.cookie('token', "", {path: '/'});
-                        parent.location.href = "../../login.html";
+                        parent.location.href = "/login.html";
                     }, 500);
                 } else {
-                    layer.alert(result.exception, {icon: 7, anim: 6});
+                    layer.alert(result.msg, {icon: 7, anim: 6});
                 }
             }
         });
